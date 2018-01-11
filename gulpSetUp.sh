a253bb45f3272a98035c26675c32443e015068a7
ccam <cam@cameronmcdonald.me>
2018-01-11 16:21:31 -0500

    init commit

---
+++ gulpSetUp.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mkdir src dist
+touch src/index.html
+mkdir src/scss src/css src/js
+touch src/scss/main.scss src/js/script.js
+cp ~/code/web/gulp/gulpfile.js .
+npm init
+npm install gulp gulp-clean-css gulp-sass gulp-server-livereload gulp-uglify sass browser-sync --save-dev
